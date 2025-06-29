7b8b4cf917a2116f5c7a738a0d56a76c23ce74e9
AdamSchlager <adam@adamschlager.com>
2025-06-29 12:01:20 -0500

    feat: configure Karma to use ChromeHeadless for CI testing

--- karma.conf.js
+++ karma.conf.js
@@ -26,7 +26,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu']
+      }
+    },
+    browsers: ['ChromeHeadlessCI'],
     singleRun: false,
     restartOnFileChange: true,
     files: [
