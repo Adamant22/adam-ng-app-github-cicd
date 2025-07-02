031aeb564284a46c06b0d52fa0f18aab0d7b5c88
AdamSchlager <adam@adamschlager.com>
2025-07-02 09:34:25 -0500

    - Fixed versioning issues refactor: downgrade Angular dependencies to version 17.3.10 and adjust related configurations
    - Updated package.json to use Angular 17.3.10 for all Angular packages.
    - Changed TypeScript version to 5.4.0.
    - Commented out unused imports and providers in app.config.ts.
    - Marked app component as standalone in app.ts.
    - Added polyfills.ts to the app's TypeScript configuration.
    - Created an empty polyfills.ts file.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,15 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+//import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig} from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+//import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideClientHydration } from '@angular/platform-browser';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    //provideBrowserGlobalErrorListeners(),
+    //provideZonelessChangeDetection(),
+    //provideRouter(routes), provideClientHydration(withEventReplay())
   ]
 };
